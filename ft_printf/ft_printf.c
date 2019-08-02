386f75a4ccdfaa322fd66ba708eaa0cf6c363b8c
Mark Lokhorst <mlokhors@f1r6s6.codam.nl>
2019-08-02 16:30:29 +0200

    first start working make file

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mlokhors <mlokhors@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/08/02 14:06:17 by mlokhors       #+#    #+#                */
+/*   Updated: 2019/08/02 16:30:08 by mlokhors      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include "../includes/libft.h"
+
+int             ft_printf(const char *str, ...)
+{
+    va_list argc;
+    
+    return (0);        
+}
