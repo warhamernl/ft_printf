ab4f365b2dcb8bc5b504654a06a852afe55c91e8
Mark Lokhorst <mlokhors@f1r6s3.codam.nl>
2019-08-06 15:02:55 +0200

    examples regular printf

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <printf.h>
+
+int     main(void)
+{
+ 	printf("The color: %s\n", "blue");
+	printf("First number: %d\n", 12345);
+	printf("Second number: %04d\n", 25);
+	printf("Third number: %i\n", 1234);
+	printf("Float number: %3.2f\n", 3.14159);
+	printf("Hexadecimal: %x\n", 255);
+	printf("Octal: %o\n", 255);
+	printf("Unsigned value: %u\n", 150);
+	printf("Just print the percentage sign %%\n");
+
+    printf(":%s:\n", "Hello, world!");
+	printf(":%15s:\n", "Hello, world!");
+	printf(":%.10s:\n", "Hello, world!");
+	printf(":%-10s:\n", "Hello, world!");
+	printf(":%-15s:\n", "Hello, world!");
+	printf(":%.15s:\n", "Hello, world!");
+	printf(":%15.10s:\n", "Hello, world!");
+	printf(":%-15.10s:\n", "Hello, world!");
+
+    printf("%                 5d\n", 10);
+    printf("%    .15     d   \n", 10);
+    printf("%d\n", 10);
+}
